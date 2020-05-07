750de80a7602867f714d2abd81c679325827efcc
Joyxi <inmema81@gmail.com>
2020-05-07 08:50:29 +0200

    ajout plante.cpp et plante.h

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include PLANTE.CPP
+#include PLANTE.H
 
 int main(){
-    
+    std::cout << "Bienvenue dans votre jardin !" << std::endl;
+
+    return 0;
 }
---
+++ plante.h
@@ -0,0 +1,15 @@
+#if !defined(__PLANTE__)
+#include <iostream>
+#define __PLANTE__
+
+class Plante{
+    private:
+        string name;
+        int taille;
+        int sante;
+        int hydratation;
+    public:
+
+}
+
+#endif
