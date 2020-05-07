e8b580c2c10c05a772abe5d3bf6cc5afa64016f9
Joyxi <inmema81@gmail.com>
2020-05-07 11:40:48 +0200

    Update botaniste.cpp

--- botaniste.cpp
+++ botaniste.cpp
@@ -17,18 +17,29 @@ void Botaniste :: dormir (int heure, Plante * cible){
 
 }
 
-void Botaniste :: acheter (){
+void Botaniste :: acheter (Plante * cible){
+    //if(*cible _taille < 3){
     _argent -= 10;
     _plantes += 1;
     if(_argent<0) _argent = 0;
     if(_argent<0) std::cout << "Vous n'avez pas assez d'argent." << std::endl;
+    //}
 }
 
-void Botaniste :: vendre(){
-    _argent += 13;
-    _plantes -= 1;
-    if(_plantes < 1) _plantes = 0;
-    if(_plantes < 1) std::cout << "Vous n'avez plus de plantes à vendre. "<< std::endl;
+void Botaniste :: vendre(Plante * cible){
+    //if(*cible _taille > 7){
+         _argent += 13;
+        _plantes -= 1;
+        if(_plantes < 1) _plantes = 0;
+        if(_plantes < 1) std::cout << "Vous n'avez plus de plantes à vendre. "<< std::endl;
+    //} 
+}
+
+void Botaniste :: buy(Plante * cible){
+    if(_argent<0) _argent = 0;
+    if(_argent<0) std::cout << "Vous n'avez pas assez d'argent." << std::endl;
+    _argent -= 5;
+    //*cible engrais += 4;
 }
 
 #endif
